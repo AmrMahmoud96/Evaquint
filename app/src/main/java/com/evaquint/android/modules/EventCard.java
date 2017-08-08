7597af374e82378d7b4ed707fbed812d726f2631
henry <henry.chen414@gmail.com>
2017-08-07 23:28:59 -0400

    draft login landing page finished
    will add the flow

--- app/src/main/java/com/evaquint/android/modules/EventCard.java
+++ app/src/main/java/com/evaquint/android/modules/EventCard.java
@@ -6,14 +6,16 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.evaquint.android.R;
 
-public class EventCard extends PercentRelativeLayout {
-    // internal components
-    private LinearLayout contentFrame;
+/**
+ * Created by henry on 8/7/2017.
+ */
 
+public class EventCard extends PercentRelativeLayout{
     public EventCard(Context context) {
         this(context, null);
     }
@@ -25,18 +27,17 @@ public class EventCard extends PercentRelativeLayout {
     public EventCard(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         LayoutInflater.from(context).inflate(R.layout.fitted_panel, this);
-        contentFrame = (LinearLayout) findViewById(R.id.content_frame);
+//        contentFrame = (LinearLayout) findViewById(R.id.content_frame);
     }
 
 
     @Override
     public void addView(View child, int index, ViewGroup.LayoutParams params){
-        if(contentFrame == null){
-            super.addView(child, index, params);
-        }else{
-            contentFrame.addView(child, index, params);
-        }
+//        if(contentFrame == null){
+//            super.addView(child, index, params);
+//        }else{
+//            contentFrame.addView(child, index, params);
+//        }
 
     }
-
 }
--- app/src/main/java/com/evaquint/android/modules/MaterialButton.java
+++ app/src/main/java/com/evaquint/android/modules/MaterialButton.java
@@ -1,20 +1,26 @@
 package com.evaquint.android.modules;
 
-import android.content.Context;
-import android.support.percent.PercentRelativeLayout;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+        import android.content.Context;
+        import android.graphics.drawable.Drawable;
+        import android.media.Image;
+        import android.support.percent.PercentRelativeLayout;
+        import android.support.v7.widget.AppCompatButton;
+        import android.util.AttributeSet;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+        import android.widget.LinearLayout;
 
-import com.evaquint.android.R;
+        import com.evaquint.android.R;
 
 /**
  * Created by henry on 8/7/2017.
  */
 
-public class MaterialButton extends PercentRelativeLayout{
+public class MaterialButton extends AppCompatButton {
+    // internal components
+    ImageView icon;
     public MaterialButton(Context context) {
         this(context, null);
     }
@@ -25,18 +31,12 @@ public class MaterialButton extends PercentRelativeLayout{
 
     public MaterialButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        LayoutInflater.from(context).inflate(R.layout.fitted_panel, this);
-//        contentFrame = (LinearLayout) findViewById(R.id.content_frame);
+//        LayoutInflater.from(context).inflate(R.layout., this);
+        icon = (ImageView) findViewById(R.id.button_icon);
+//        icon.setImageDrawable(new Drawable() {
+//        });
     }
 
 
-    @Override
-    public void addView(View child, int index, ViewGroup.LayoutParams params){
-//        if(contentFrame == null){
-//            super.addView(child, index, params);
-//        }else{
-//            contentFrame.addView(child, index, params);
-//        }
 
-    }
 }
