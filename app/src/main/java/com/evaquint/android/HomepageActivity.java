024c26195e0998639e6fe7ee72ee2e611df2b1ed
Kareem Mohamed <kareem.mohamed@mail.utoronto.ca>
2017-10-01 17:52:19 -0400

    Added Map and Messaging Activity, they are both connected by buttons

--- app/src/main/java/com/evaquint/android/HomepageActivity.java
+++ app/src/main/java/com/evaquint/android/HomepageActivity.java
@@ -1,7 +1,9 @@
 package com.evaquint.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class HomepageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
     }
+    public void goToMap(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/evaquint/android/MapActivity.java
@@ -0,0 +1,27 @@
+package com.evaquint.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        Intent intent = getIntent();
+        intent = new Intent(this, MainActivity.class);
+        setContentView(R.layout.activity_map);
+    }
+
+    public void goToMessaging(View view) {
+        Intent intent = new Intent(this, MessagingActivity.class);
+        startActivity(intent);
+    }
+    public void goToHomepage(View view) {
+        Intent intent = new Intent(this, HomepageActivity.class);
+        startActivity(intent);
+    }
+}
