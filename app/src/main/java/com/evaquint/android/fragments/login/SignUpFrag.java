18881e9b509e666cce3dd331b2a9d31f589d7850
Amr <amrmahmoud.am@hotmail.com>
2017-11-13 21:35:37 -0500

    Added functionality to signup and changed some of layout/id for the new register fragment. needs to have password and email validity check as well as more intense checking of the database if stuff exists or not, etc.

--- app/src/main/java/com/evaquint/android/fragments/login/SignUpFrag.java
+++ app/src/main/java/com/evaquint/android/fragments/login/SignUpFrag.java
@@ -2,18 +2,32 @@ package com.evaquint.android.fragments.login;
 
 import android.app.Activity;
 import android.app.LoaderManager;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import com.evaquint.android.MainActivity;
 import com.evaquint.android.R;
+import com.evaquint.android.fragments.map.EventLocatorFrag;
 import com.evaquint.android.utils.view.ViewAnimator;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.text.Text;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import static com.evaquint.android.utils.view.FragmentHelper.setActiveFragment;
 
@@ -24,13 +38,32 @@ import static com.evaquint.android.utils.view.FragmentHelper.setActiveFragment;
 public class SignUpFrag extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     private View view;
     private Activity activity;
+    private EditText mNameField;
+    private EditText mEmailField;
+    private EditText mPasswordField;
 
+    private Button mRegisterBtn;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDataBase;
+
+   // private ProgressBar mProgress;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.view = inflater.inflate(R.layout.fragment_register, container, false);
         this.activity = getActivity();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDataBase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mNameField = (EditText) view.findViewById(R.id.nameField);
+        mEmailField =(EditText) view.findViewById(R.id.emailField);
+        mPasswordField = (EditText) view.findViewById(R.id.passwordField);
+       // mProgress = new ProgressBar(activity, null,android.R.attr.progressBarStyleSmall)
+        mRegisterBtn = (Button) view.findViewById(R.id.registerBtn);
 
         final Button mSwitchButton = (Button) view.findViewById(R.id.switch_button);
         mSwitchButton.setOnClickListener(new View.OnClickListener() {
@@ -39,13 +72,47 @@ public class SignUpFrag extends Fragment implements LoaderManager.LoaderCallback
                 setActiveFragment(SignUpFrag.this, new SignInFrag());
             }
         });
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startRegister();
+            }
+        });
         // Set up everything else
 
 
         return this.view;
     }
 
+    public void startRegister(){
+        final String name = mNameField.getText().toString().trim();
+        String email = mEmailField.getText().toString().trim();
+        String password = mPasswordField.getText().toString().trim();
 
+        if(!TextUtils.isEmpty(name)&& !TextUtils.isEmpty(email)&& !TextUtils.isEmpty(password)){
+
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+
+                        String user_id = mAuth.getCurrentUser().getUid();
+
+                       DatabaseReference current_user_db = mDataBase.child(user_id);
+
+                        current_user_db.child("name").setValue(name);
+                        current_user_db.child("image").setValue("default");
+                        Intent mainIntent = new Intent(activity, MainActivity.class);
+                       startActivity(mainIntent);
+
+                    }
+
+                }
+            });
+
+        }
+    }
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         return null;
